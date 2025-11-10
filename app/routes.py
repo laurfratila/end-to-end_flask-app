dafb206e4c2792b7f869b279316eb1b60dd612f8
Laurentiu Fratila <laurentiu.fratila@endava.com>
2025-11-10 13:39:53 +0200

    simple web page rendering

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Laur'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Tero'},
+            'body': 'Cu dronele faci treaba !'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
