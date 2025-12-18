634015d95b597786ee40fe186ac2a1c115646cd8
Laurentiu Fratila <laurentiu.fratila@endava.com>
2025-12-18 12:02:08 +0200

    configured server mail to sent real emails

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+def str_to_bool(value: str, default=False):
+    if value is None:
+        return default
+    return value.strip().lower() in ("1", "true", "t", "yes", "y")
+
+
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -15,17 +22,13 @@ class Config:
 
      # --- Email config ---
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'localhost')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', 1025))  # MailHog listens on 1025
-
-    # Force NO TLS/SSL for MailHog
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = False
-
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
+    MAIL_USE_TLS = str_to_bool(os.environ.get('MAIL_USE_TLS'), default=False)
+    MAIL_USE_SSL = str_to_bool(os.environ.get('MAIL_USE_SSL'), default=False)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'no-reply@microblog.local')
-    ADMINS = ['your-email@example.com']
-
+    ADMINS = [os.environ.get('ADMIN_EMAIL', 'your-email@example.com')]
 
     LANGUAGES = ['en', 'es', 'ro']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
